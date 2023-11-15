57e89af8d89cfe5e3ea24481dca3ff735e5ec014
KikuTi <matheus.ksousa@sptech.school>
2023-11-14 23:54:42 -0300

    acertando alinhamento de texto e colocando icone

---
+++ dashboard/index.js
@@ -0,0 +1,179 @@
+
+
+const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [
+            {
+                label: 'temperatura',
+                data: [12, 19, 3, 5, 2, 3],
+                borderWidth: 2,
+                backgroundColor: "red",
+                borderColor: "red",
+            },
+            {
+                label: 'umidade',
+                data: [12, 19, 23, 5, 2, 3],
+                borderWidth: 2,
+                backgroundColor: "#4761f4",
+                borderColor: "#4761f4",
+            },
+        ]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+const ctx2 = document.getElementById('myChart2');
+
+new Chart(ctx2, {
+    type: 'line',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [
+            {
+                label: 'temperatura',
+                data: [12, 19, 3, 5, 2, 3],
+                borderWidth: 2,
+                backgroundColor: "red",
+                borderColor: "red",
+            },
+            {
+                label: 'umidade',
+                data: [12, 19, 23, 5, 2, 3],
+                borderWidth: 2,
+                backgroundColor: "#4761f4",
+                borderColor: "#4761f4",
+            },
+        ]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+const ctx3 = document.getElementById('myChart3');
+
+new Chart(ctx3, {
+    type: 'line',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [
+            {
+                label: 'temperatura',
+                data: [12, 19, 3, 5, 2, 3],
+                borderWidth: 2,
+                backgroundColor: "red",
+                borderColor: "red",
+            },
+            {
+                label: 'umidade',
+                data: [12, 19, 23, 5, 2, 3],
+                borderWidth: 2,
+                backgroundColor: "#4761f4",
+                borderColor: "#4761f4",
+            },
+        ]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+
+var sala = 0;
+var primeiroRack = 0;
+function fecharSalas() {
+
+    document.getElementById("inputSalas").style.display = 'none'
+
+    if (sala == 0) {
+        document.querySelector('.main_sala').style.display = 'flex';
+        document.querySelector('.main_salas').style.display = 'none';
+        document.querySelector('.salas_titulo').style.display = 'none';
+        headerTitulo.innerHTML = 'Sala 1';
+        sala++;
+    } else {
+        document.querySelector('.main_sala').style.display = 'none';
+        document.querySelector('.main_salas').style.display = 'flex';
+        document.querySelector('.salas_titulo').style.display = 'flex';
+        headerTitulo.innerHTML = 'Salas';
+        sala--;
+    }
+}
+function rack1() {
+
+    if (primeiroRack == 0) {
+        primeiroRack++;
+        document.getElementById("rackDois").style.display = 'none';
+        document.getElementById("rackTres").style.display = 'none';
+        document.getElementById("rackUm").style.height = '95%';
+        document.getElementById("rackMain").style.height = '32%';
+        document.getElementById("varSensores").style.display = 'block';
+        document.getElementById("graficosRacks").style.display = 'flex';
+    } else {
+        document.getElementById("rackDois").style.display = 'block';
+        document.getElementById("rackTres").style.display = 'block';
+        document.getElementById("rackUm").style.height = '30%';
+        document.getElementById("rackMain").style.height = '100%';
+        document.getElementById("varSensores").style.display = 'none';
+        document.getElementById("graficosRacks").style.display = 'none';
+        primeiroRack--;
+    }
+}
+function rack2() {
+
+    if (primeiroRack == 0) {
+        primeiroRack++;
+        document.getElementById("rackUm").style.display = 'none';
+        document.getElementById("rackTres").style.display = 'none';
+        document.getElementById("rackDois").style.height = '95%';
+        document.getElementById("rackMain2").style.height = '32%';
+        document.getElementById("varSensores2").style.display = 'block';
+        document.getElementById("graficosRacks2").style.display = 'flex';
+    } else {
+        document.getElementById("rackUm").style.display = 'block';
+        document.getElementById("rackTres").style.display = 'block';
+        document.getElementById("rackDois").style.height = '30%';
+        document.getElementById("rackMain2").style.height = '100%';
+        document.getElementById("varSensores2").style.display = 'none';
+        document.getElementById("graficosRacks2").style.display = 'none';
+        primeiroRack--;
+    }
+}
+function rack3() {
+
+    if (primeiroRack == 0) {
+        primeiroRack++;
+        document.getElementById("rackDois").style.display = 'none';
+        document.getElementById("rackUm").style.display = 'none';
+        document.getElementById("rackTres").style.height = '95%';
+        document.getElementById("rackMain3").style.height = '32%';
+        document.getElementById("varSensores3").style.display = 'block';
+        document.getElementById("graficosRacks3").style.display = 'flex';
+    } else {
+        document.getElementById("rackDois").style.display = 'block';
+        document.getElementById("rackUm").style.display = 'block';
+        document.getElementById("rackTres").style.height = '30%';
+        document.getElementById("rackMain3").style.height = '100%';
+        document.getElementById("varSensores3").style.display = 'none';
+        document.getElementById("graficosRacks3").style.display = 'none';
+        primeiroRack--;
+    }
+}
